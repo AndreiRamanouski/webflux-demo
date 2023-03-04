aa92231ebb8f4ef5e7e1d5ac9cfc81547375da2c
A Romanouski <a.romanouski@tapston.com>
2023-03-04 08:42:41 +0300

    [MONO-FLUX] Implement CRUD with Mysql/Oracle - add main class and entities

---
+++ reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/ReactiveMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.reactive.demo.reactive.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMysqlApplication.class, args);
+    }
+}
---
+++ reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/entity/User.java
@@ -0,0 +1,25 @@
+package com.reactive.demo.reactive.mysql.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table
+@Accessors(chain = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private Long id;
+    private String userId;
+    private String subscriptionId;
+    private String status;
+}
