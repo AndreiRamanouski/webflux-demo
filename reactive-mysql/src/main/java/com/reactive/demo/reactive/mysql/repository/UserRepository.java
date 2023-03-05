a46e9daa72dc88b8c9bafa7f9684e82d00037a09
A Romanouski <a.romanouski@tapston.com>
2023-03-05 07:37:56 +0300

    [MONO-FLUX] Implement CRUD with Mysql/Oracle - implement user service

--- reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/repository/UserRepository.java
+++ reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.reactive.demo.reactive.mysql.repository;
 import com.reactive.demo.reactive.mysql.entity.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
 
+    Flux<User> findAllByUserId(String userId);
 }
--- reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/service/impl/UserServiceImpl.java
+++ reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/service/impl/UserServiceImpl.java
@@ -1,31 +1,44 @@
 package com.reactive.demo.reactive.mysql.service.impl;
 
 import com.reactive.demo.reactive.mysql.dto.UserDto;
+import com.reactive.demo.reactive.mysql.mapper.UserMapper;
+import com.reactive.demo.reactive.mysql.repository.UserRepository;
 import com.reactive.demo.reactive.mysql.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public Mono<UserDto> saveUser(Mono<UserDto> userDto) {
-        return null;
+        log.info("saveUser");
+        return userDto.map(UserMapper::mapDtoToEntity).flatMap(userRepository::save).map(UserMapper::mapEntityToDto);
     }
 
     @Override
     public Mono<UserDto> updateUser(Mono<UserDto> userDto, Long id) {
-        return null;
+        log.info("updateUser");
+        return userRepository.findById(id).flatMap(user -> userDto.map(UserMapper::mapDtoToEntity))
+                .doOnNext(user -> user.setId(id)).map(UserMapper::mapEntityToDto);
     }
 
     @Override
     public Mono<Void> deleteUser(Long id) {
-        return null;
+        log.info("deleteUser");
+        return userRepository.deleteById(id);
     }
 
     @Override
     public Flux<UserDto> getAllByUserId(String userId) {
-        return null;
+        log.info("getAllByUserId");
+        return userRepository.findAllByUserId(userId).map(UserMapper::mapEntityToDto);
     }
 }
