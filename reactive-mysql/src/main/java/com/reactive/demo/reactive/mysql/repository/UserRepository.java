532457b1d8acb1e1c3d4d7335107a8e162c9df2e
A Romanouski <a.romanouski@tapston.com>
2023-03-08 08:43:22 +0300

    [MONO-FLUX] Implement CRUD with Mysql/Oracle - add update email

--- reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/repository/UserRepository.java
+++ reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/repository/UserRepository.java
@@ -1,12 +1,19 @@
 package com.reactive.demo.reactive.mysql.repository;
 
 import com.reactive.demo.reactive.mysql.entity.User;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
 
     Flux<User> findAllByUserId(String userId);
+
+    Mono<User> findByUserIdAndDeviceId(String userId, String deviceId);
+
+    @Query("UPDATE users SET email = :email WHERE user_id = :userId AND device_id = :deviceId")
+    Mono<Void> updateEmail(String email, String userId, String deviceId);
 }
