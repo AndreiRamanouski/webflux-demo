6be96cde8c46ad625f25d5dac8cac520d127208d
A Romanouski <a.romanouski@tapston.com>
2023-03-09 07:49:40 +0300

    [MONO-FLUX] Spring email - implement email sending

--- reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/service/impl/EmailNotificationServiceImpl.java
+++ reactive-mysql/src/main/java/com/reactive/demo/reactive/mysql/service/impl/EmailNotificationServiceImpl.java
@@ -6,6 +6,8 @@ import com.reactive.demo.reactive.mysql.service.EmailNotificationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -14,14 +16,31 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class EmailNotificationServiceImpl implements EmailNotificationService {
 
+    private final JavaMailSender javaMailSender;
+
     @Override
-    public void sendSingleEmail(EmailNotificationRequest email, Mono<User> user) {
+    public void sendSingleEmail(EmailNotificationRequest emailRequest, Mono<User> userRequest) {
         log.info("sendSingleEmail");
+        SimpleMailMessage simpleMailMessage = getSimpleMailMessage(emailRequest);
+        simpleMailMessage.setTo(userRequest.map(User::getEmail).toString());
+        javaMailSender.send(simpleMailMessage);
     }
 
 
     @Override
-    public void sendEmails(List<String> emails, EmailNotificationRequest emailNotificationRequest){
+    public void sendEmails(List<String> emails, EmailNotificationRequest emailNotificationRequest) {
         log.info("sendEmails");
+        SimpleMailMessage simpleMailMessage = getSimpleMailMessage(emailNotificationRequest);
+        emails.forEach((e) -> {
+            simpleMailMessage.setTo(e);
+            javaMailSender.send(simpleMailMessage);
+        });
+    }
+
+    private static SimpleMailMessage getSimpleMailMessage(EmailNotificationRequest emailRequest) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setSubject(emailRequest.getTitle());
+        simpleMailMessage.setText(emailRequest.getBody());
+        return simpleMailMessage;
     }
 }
